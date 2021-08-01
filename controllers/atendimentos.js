b85c60221441ff6c8d5e96de56615229f290078a
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-08-01 14:00:58 -0300

    custom express configurado

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -5,4 +5,14 @@ module.exports = app => {
 
     app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um get'))
 
+
+    app.post('/atendimentos',
+    (req, res) => {
+        console.log(req.body)
+        res.send('voce esta na rota de antendimentoe esta realizando um     POST')
+    }
+    
+    
+     )
+
 }
